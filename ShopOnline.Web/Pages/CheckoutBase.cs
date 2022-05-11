8f9c207055b716d5d2abfe05639d407ddd1851cb
Marcos <marquinhooliveira9968@outlook.com>
2022-05-11 13:56:49 -0300

    add implementação com paypal

---
+++ ShopOnline.Web/Pages/CheckoutBase.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using ShopOnline.Models.Dtos;
+using ShopOnline.Web.Services.Contracts;
+
+namespace ShopOnline.Web.Pages
+{
+    public class CheckoutBase : ComponentBase
+    {
+        [Inject]
+        public IJSRuntime Js { get; set; }
+
+        protected IEnumerable<CartItemDtos> ShoppingCartItems { get; set; }
+        protected int TotalQty { get; set; }
+        protected string PaymentDescription { get; set; }
+        protected decimal PaymentAmount { get; set; }
+
+        [Inject]
+        public IShoppingCartService ShoppingCartService { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            try
+            {
+                ShoppingCartItems = await ShoppingCartService.GetItems(HardCoded.UserId);
+
+                if (ShoppingCartService != null)
+                {
+                    Guid orderGuid = Guid.NewGuid();
+
+                    PaymentAmount = ShoppingCartItems.Sum(p => p.TotalPrice);
+                    TotalQty = ShoppingCartItems.Sum(p => p.Qty);
+                    PaymentDescription = $"O_{HardCoded.UserId}_{orderGuid}";
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            try
+            {
+                if (firstRender)
+                {
+                    await Js.InvokeVoidAsync("initPayPalButton");
+                }
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
